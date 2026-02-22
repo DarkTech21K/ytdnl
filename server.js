a7bfc7b9844493e706c4080f5f13bb19070be61b
DarkTech21K <81114890+DarkTech21K@users.noreply.github.com>
2026-02-22 21:48:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,18 +7,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// De route die je app aanroept
+// NIEUW: Vertel de server om je HTML/CSS/JS bestanden te serveren
+app.use(express.static(path.join(__dirname, ''))); 
+
+// De homepage route (lost "Cannot GET /" op)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Je bestaande download route
 app.get('/download', async (req, res) => {
     const videoUrl = req.query.url;
-
-    if (!videoUrl) {
-        return res.status(400).send('Geen URL opgegeven');
-    }
+    if (!videoUrl) return res.status(400).send('Geen URL opgegeven');
 
     try {
         res.header('Content-Disposition', 'attachment; filename="audio.mp3"');
-        
-        // We streamen de audio direct naar de gebruiker
         await ytpb(videoUrl, {
             extractAudio: true,
             audioFormat: 'mp3',
@@ -27,10 +30,8 @@ app.get('/download', async (req, res) => {
         .then(output => {
             output.stdout.pipe(res);
         });
-
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Er ging iets mis bij het omzetten.');
+        res.status(500).send('Er ging iets mis.');
     }
 });
 
